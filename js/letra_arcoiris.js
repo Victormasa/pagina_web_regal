ab0eb98e76d8262baa260268dbb2733346e0ad32
PelochoV <vimartinezsantos@gmail.com>
2023-06-07 00:40:13 +0200

    a

---
+++ js/letra_arcoiris.js
@@ -0,0 +1,6 @@
+setInterval(function() {
+  var rainbowText = document.querySelector('.rainbow-text');
+  rainbowText.style.animation = 'none';
+  void rainbowText.offsetWidth; // Trigger a reflow
+  rainbowText.style.animation = null;
+}, 500);
--- js/main.js
+++
@@ -1,6 +0,0 @@
-setInterval(function() {
-  var rainbowText = document.querySelector('.rainbow-text');
-  rainbowText.style.animation = 'none';
-  void rainbowText.offsetWidth; // Trigger a reflow
-  rainbowText.style.animation = null;
-}, 500);
